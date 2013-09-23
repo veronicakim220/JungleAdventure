02e015d69130637f6f2d10bdbc1d466b0722bfff
sophia <scastellarin95@gmail.co>
2013-09-22 23:00:30 -0400

    set up the screen so stuff can happen

--- JungleAdventure.py
+++ JungleAdventure.py
@@ -1,1 +1,24 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python2
+
+import pygame as pg
+
+white = (255, 255, 255)
+black = (0,0,0)
+game = True;	    # flag
+
+pg.init()   # initialize pygame
+
+# set up the screen
+pg.display.set_caption('Jungle Adventure')
+size = [600, 400]
+scr = pg.display.set_mode(size)
+
+# main game loop
+while (game):
+
+    # Checks for keyboard input
+    for event in pg.event.get():	# checks pygame events
+	if event.type == pg.KEYDOWN:	# in the case of a keydown event
+	    if event.key == pg.K_q:	# if the key is q
+		game = False		# quit the current game
+
